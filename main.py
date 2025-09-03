52d8d5967684789fa2171f972352d1335baa66a1
Det-Sun <sonia.ramrez7@gmail.com>
2025-09-03 16:50:41 -0600

    p4

---
+++ main.py
@@ -0,0 +1,43 @@
+from gato import *
+from minmax import *
+
+
+
+printBoard(board)
+print("Computer goes first! Good luck.")
+print("Positions are as follow:")
+print("1, 2, 3 ")
+print("4, 5, 6 ")
+print("7, 8, 9 ")
+print("\n")
+
+def compMove():
+    
+    bestScore = -math.inf
+    bestMove = 0
+    
+    for key in board.keys():
+        if (board[key] == ' '):
+            board[key] = bot
+            
+            score = minimax(bot, player, board, 0, False)
+            board[key] = ' '
+            if (score > bestScore):
+                bestScore = score
+                bestMove = key
+
+    insertLetter(bot, bestMove)
+    return
+
+def playerMove():
+    position = int(input("Enter the position for 'O':  "))
+    insertLetter(player, position)
+    return
+
+global firstComputerMove
+firstComputerMove = True
+
+while not checkForWin():
+    compMove()
+    playerMove()
+
--- minmax.py
+++ minmax.py
@@ -1,51 +1,37 @@
-import numpy as np
+import math
+from gato import checkWhichMarkWon, checkDraw, board
 
-def max_valor(game, state, alpha=float('-inf'), beta=float('inf')):
-    """
-    Valores de los movimientos de Max
-    """
-    if game.is_terminal(state):
-        #Si el estado es terminal, devolvemos el valor del juego
-        return game.utility(state), None
-    
-    # Inicializamos el valor máximo como negativo infinito
-    max_value = float('-inf')
-    for action in game.actions(state):
-        next_state = game.result(state, action)
-        value = min_valor(game, next_state, alpha, beta)
-        max_value = max(max_value, value)
-        alpha = max(alpha, max_value)
-        if beta <= alpha:
-            break # Poda
-    
-    return max_value, action
 
-def min_valor(game, state, alpha=float('-inf'), beta=float('inf')):
-    """
-    Valores de los movimientos de Min
-    """
-    if game.is_terminal(state):
-        #Si el estado es terminal, devolvemos el valor del juego
-        return game.utility(state), None
-    
-    # Inicializamos el valor mínimo como positivo infinito
-    min_value = float('inf')
-    for action in game.actions(state):
-        next_state = game.result(state, action)
-        value = max_valor(game, next_state, alpha, beta)
-        min_value = min(min_value, value)
-        beta = min(beta, min_value)
-        if beta <= alpha:
-            break # Poda
-    
-    return min_value, action
 
-def minimax(game):
-    """
-    Algoritmo Minimax con poda Alpha-Beta
-    """
-    state = game.initial_state()
-    jugadas = []
-    value, move = max_valor(game, state)
-    while True:
-        
+def minimax(bot, player, board, depth, isMaximizing):
+    alpha = -math.inf
+    beta = math.inf
+    if (checkWhichMarkWon(bot)):
+        return 1
+    elif (checkWhichMarkWon(player)):
+        return -1
+    elif (checkDraw()):
+        return 0
+
+    if (isMaximizing):
+        for key in board.keys():
+            if (board[key] == ' '):
+                board[key] = bot
+                tempAlpha = minimax(bot, player, board, depth + 1, False)
+                board[key] = ' '
+                if (tempAlpha > alpha):
+                    alpha = tempAlpha
+        return alpha
+
+    else:
+        bestScore = math.inf #beta
+        for key in board.keys():
+            if (board[key] == ' '):
+                board[key] = player
+                score = minimax(bot, player, board, depth + 1, True)
+                board[key] = ' '
+                if (score < bestScore):
+                    bestScore = score
+        return bestScore
+
+
